88410fd48b9d39d2b00f23f0ae680f7a02105c62
SeverinDK <some12thing@gmail.com>
2016-03-03 12:22:12 +0100

    Update imagefader.js
    Added a randomize bool, so you can get random images or next image in the list.

--- imagefader.js
+++ imagefader.js
@@ -1,94 +1,67 @@
-(function() {
+(function () {
 
-	/*************************************************************************************************
+    /*************************************************************************************************
 			Add images to fade in this array. Can also be in folders, just include folderpath.
 	*************************************************************************************************/
-	var images = [
-		"image1.jpg",
-		"image2.jpg",
-		"image3.jpg",
-		"image4.jpg",
-		"image5.jpg",
-	];
+    var images = [
+		"http://placehold.it/100x100",
+		"http://placehold.it/200x200",
+		"http://placehold.it/300x300",
+		"http://placehold.it/400x400",
+		"http://placehold.it/500x500",
+    ];
 
-	var parent = "#img-container";	// Det element der skal indeholde billederne. Kan være hvilket som helst element. Body, divs. osv.
-	var showTime = 2500;			// Den tid hver billede vil blive vist. (Millisekunder)
-	var fadeTime = 1000;			// Den tid det tager at fade fra det sidste viste billede til det nye. (Millisekunder)
-
-
-
+    var parent = "#img-container";
+    var showTime = 2500;
+    var fadeTime = 1000;			
+    var randomize = false;
 
 
 
+    var front; var back; var active;
+    var imageIndex = -1;
+    var faded = false;
 
+    $(document).ready(function () {
+        initImageFader();
+        fade(showTime);
+    });
 
+    function initImageFader() {
+        front = document.createElement("img");
+        back = document.createElement("img");
+        $(parent).append(back);
+        $(parent).append(front);
+        $(front).attr("src", getNextImage());
+        $(back).attr("src", getNextImage());
+        $(parent + " img").css("position", "fixed");
+    }
 
+    function fade(delay) {
+        setInterval(function () {
+            var opacity = faded ? 1 : 0;
+            $(front).animate({
+                "opacity": opacity
+            }, fadeTime, function () {
+                faded = !faded;
+                active = faded ? front : back;
+                if (randomize) {
+                    $(active).attr("src", getRandomImage());
+                } else {
+                    $(active.attr("src", getNextImage()));
+                }
+            });
+        }, delay);
+    }
 
+    function getNextImage() {
+        imageIndex++;
+        imageIndex = imageIndex < images.length ? imageIndex : 0;
+        return images[imageIndex];
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	/*************************************************************************************************
-			DONT TOUCH CODE BELOW!.. Unless you know what you are doing :P
-	*************************************************************************************************/
-	var front; var back; var active;
-	var imageIndex = -1;
-	var faded = false;
-
-	$(document).ready(function() {
-		initImageFader();
-		fade(showTime);
-	});
-
-	function initImageFader() {
-		front = document.createElement("img");
-		back = document.createElement("img");
-		$(parent).append(back);
-		$(parent).append(front);
-		$(front).attr("src", getNextImage());
-		$(back).attr("src", getNextImage());
-		$(parent + " img").css("position", "fixed");
-	}
-
-	function fade(delay) {
-		setInterval(function() {
-			var opacity = faded ? 1 : 0;
-			$(front).animate({
-				"opacity": opacity
-			}, fadeTime, function() {
-				faded = !faded;
-				active = faded ? front : back;
-				$(active).attr("src", getNextImage());
-			});
-		}, delay);
-	}
-
-	function getNextImage() {
-		imageIndex++;
-		imageIndex = imageIndex < images.length ? imageIndex : 0;
-		return images[imageIndex];
-	}
+    function getRandomImage() {
+        return images[Math.round(Math.random() * images.length)];
+    }
 
 })();
