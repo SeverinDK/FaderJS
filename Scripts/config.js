c6aea34bcef004ab12223ddf827c63545c20c78b
SeverinDK <some12thing@gmail.com>
2016-09-30 02:32:13 +0200

    minor changes

--- Scripts/config.js
+++ Scripts/config.js
@@ -1,6 +1,3 @@
 ﻿requirejs.config({
-    baseUrl: 'Scripts',
-    paths: {
-        jquery: 'jquery-3.1.1.min',
-    }
+    baseUrl: 'Scripts'
 });
---
+++ test.js
@@ -0,0 +1,19 @@
+﻿require(['fader'], function () {
+
+    var fader = new Fader(document.getElementById("image-container"), [
+        "http://www.placehold.it/400x150/000000",
+        "http://www.placehold.it/400x150/ffffff",
+    ]);
+
+    fader.start();
+
+    $("#image-container").mouseenter(function () {
+        fader.stop();
+        console.log("stopped.");
+    });
+
+    $("#image-container").mouseleave(function () {
+        fader.start();
+        console.log("resumed.");
+    });
+});
